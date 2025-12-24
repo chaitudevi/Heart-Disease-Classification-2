772ea503573c59ac2b7346a3de33bde3a1cd9c5e
mlwithak <amrithakdas.work@gmail.com>
2025-12-24 12:27:08 +0530

    Adding setup file

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name="heart_disease_mlops",
     version="0.1",
-    packages=find_packages(where="src"),
+    packages=find_packages(),
     package_dir={"": "src"},
 )
